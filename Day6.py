4318efc86b295fad3dbc156d1f3b01d378455e8e
Gouda1281 <basavanagoudahbh.gmail.com>
2025-08-12 14:58:16 -0400

    adding Day 5,6,7 and main file changes

---
+++ Day6.py
@@ -0,0 +1,11 @@
+
+def function():
+    print("created functions")
+    print("moving out of the function")
+ 
+
+
+def Day6_learning() :
+    print("Day6 Functions Codeblocks and while loops ")
+
+    function()
